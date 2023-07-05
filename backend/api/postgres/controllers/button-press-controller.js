9dafb1c94f33a7aa380c7280f9400d2a3378f225
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-05 11:29:55 -0700

    Refactor button-press-controller

--- backend/api/postgres/controllers/button-press-controller.js
+++ backend/api/postgres/controllers/button-press-controller.js
@@ -2,56 +2,31 @@ const db = require("../models");
 const Button_Press = db.button_presses;
 const Op = db.Sequelize.Op;
 
-// module.exports = {
-//     create: function(req, res) {
-//         // Validate request
-//         if (!req.body.title) {
-//           res.status(400).send({
-//             message: "Content can not be empty!"
-//           });
-//           return;
-//         }
-//         // Create a Tutorial
-//         const button_press = {
-//           user: req.body.user,
-//           time: req.body.time
-//         };
-//         // Save Tutorial in the database
-//         Button_Press.create(button_press)
-//           .then(data => {
-//             res.send(data);
-//           })
-//           .catch(err => {
-//             res.status(500).send({
-//               message:
-//                 err.message || "Some error occurred while creating the Tutorial."
-//             });
-//           });
-//     }
-// }
-exports.create = (req, res) => {
-  console.log("======Button press recorded======");
-    // Validate request
-    if (!req.body.student) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    }
-    // Create a Tutorial
-    const press_data = {
-      student: req.body.student,
-    };
-    // Save Tutorial in the database
-    Button_Press.create(press_data)
-      .then(data => {
-        console.log(data);
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the Button Press."
-        });
-      });
-  };
+module.exports = {
+    create: function(req, res) {
+      console.log("======Button press recorded======");
+        // Validate request
+        if (!req.body.student) {
+          res.status(400).send({
+            message: "Content can not be empty!"
+          });
+          return;
+        }
+        // Create a Tutorial
+        const press_data = {
+          student: req.body.student,
+        };
+        // Save Tutorial in the database
+        Button_Press.create(press_data)
+          .then(data => {
+            console.log(data);
+            res.send(data);
+          })
+          .catch(err => {
+            res.status(500).send({
+              message:
+                err.message || "Some error occurred while creating the Button Press."
+            });
+          });
+        }
+}
