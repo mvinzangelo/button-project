4b1d51864b21a82bd2504671a654e9524f6e74cf
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-05 11:44:52 -0700

    Add lecture to button-press-controller

--- backend/api/postgres/controllers/button-press-controller.js
+++ backend/api/postgres/controllers/button-press-controller.js
@@ -1,32 +1,33 @@
 const db = require("../models");
 const Button_Press = db.button_presses;
+const Lecture = db.lectures;
 const Op = db.Sequelize.Op;
 
 module.exports = {
-    create: function(req, res) {
-      console.log("======Button press recorded======");
-        // Validate request
-        if (!req.body.student) {
-          res.status(400).send({
-            message: "Content can not be empty!"
-          });
-          return;
-        }
-        // Create a Tutorial
-        const press_data = {
-          student: req.body.student,
-        };
-        // Save Tutorial in the database
-        Button_Press.create(press_data)
-          .then(data => {
-            console.log(data);
-            res.send(data);
-          })
-          .catch(err => {
-            res.status(500).send({
-              message:
-                err.message || "Some error occurred while creating the Button Press."
-            });
-          });
-        }
+  create: function (req, res) {
+    console.log("======Button press recorded======");
+    // Validate request
+    if (!req.body.student) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+    // Create a Tutorial
+    const press_data = {
+      student: req.body.student,
+    };
+    // Save Tutorial in the database
+    Button_Press.create(press_data)
+      .then(data => {
+        console.log(data);
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Button Press."
+        });
+      });
+  }
 }
