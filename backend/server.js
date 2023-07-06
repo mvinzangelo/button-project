afaf41a2f740f5329a9a3af39ec3f0feca876a71
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-06 01:07:36 -0700

    Add socket to server.js and connect on frontend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const db = require("./api/postgres/models")
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors');
 const { Server } = require("socket.io");
 
 const middleware = require('./middleware')
@@ -15,6 +16,7 @@ const zoomAppRouter = require('./api/zoomapp/router')
 const zoomRouter = require('./api/zoom/router')
 const thirdPartyOAuthRouter = require('./api/thirdpartyauth/router')
 const postgresRouter = require('./api/postgres/router')
+const { env } = require('process')
 // Create app
 const app = express()
 
@@ -30,6 +32,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(middleware.session)
 app.use(middleware.setResponseHeaders)
+app.use(cors());
 
 // Zoom App routes
 app.use('/api/zoomapp', zoomAppRouter)
@@ -72,40 +75,40 @@ app.use((error, req, res) => {
 // Start express server
 const server = http.createServer(app).listen(process.env.PORT, () => {
   console.log('Zoom App is listening on port', process.env.PORT)
-})
-
-// Create connection to db
-// ! {force: true} for development purposes only
-db.sequelize.sync({ force: true }).then(async () => {
-  // ! test functions
-  const lecture_controller = require('./api/postgres/controllers/lecture-controller');
-  const button_press_controller = require('./api/postgres/controllers/button-press-controller');
-
-  const lect1 = await lecture_controller.createLecture({
-    instructor: "Test Instructor 1",
-  });
-  const lect2 = await lecture_controller.createLecture({
-    instructor: "Test Instructor 2",
-  });
-  const button_press1 = await button_press_controller.createButtonPress(lect1.id, {
-    student: "Test Student 1",
-  });
-  const button_press3 = await button_press_controller.createButtonPress(lect1.id, {
-    student: "Test Student 3",
-  });
-  const button_press2 = await button_press_controller.createButtonPress(lect2.id, {
-    student: "Test Student 2",
-  });
-  const all = await lecture_controller.findAll();
-  // ! test functions
-  console.log("Sync'd successfully.");
-}).catch((error) => {
-  console.error("Unable to sync : ", error);
 });
 
 // initialize socket.io
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-  console.log("user connected");
+  console.log(`SOCKET: user connected ${socket.id}`);
 })
+
+// Create connection to db
+// ! {force: true} for development purposes only
+db.sequelize.sync({ force: true }).then(async () => {
+  // ! test functions
+  // const lecture_controller = require('./api/postgres/controllers/lecture-controller');
+  // const button_press_controller = require('./api/postgres/controllers/button-press-controller');
+
+  // const lect1 = await lecture_controller.createLecture({
+  //   instructor: "Test Instructor 1",
+  // });
+  // const lect2 = await lecture_controller.createLecture({
+  //   instructor: "Test Instructor 2",
+  // });
+  // const button_press1 = await button_press_controller.createButtonPress(lect1.id, {
+  //   student: "Test Student 1",
+  // });
+  // const button_press3 = await button_press_controller.createButtonPress(lect1.id, {
+  //   student: "Test Student 3",
+  // });
+  // const button_press2 = await button_press_controller.createButtonPress(lect2.id, {
+  //   student: "Test Student 2",
+  // });
+  // const all = await lecture_controller.findAll();
+  // ! test functions
+  console.log("Sync'd successfully.");
+}).catch((error) => {
+  console.error("Unable to sync : ", error);
+});
