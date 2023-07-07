3cf9831ead8725eeeda86bf7d48a2cc62c2ea0e2
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-07 15:29:00 -0700

    Add argument for lectureStore

--- backend/api/postgres/handers/professorHandler.js
+++ backend/api/postgres/handers/professorHandler.js
@@ -1,6 +1,6 @@
 const lecture_controller = require('../controllers/lecture-controller');
 
-module.exports = (io, socket) => {
+module.exports = (io, socket, lectures) => {
     const onCreatenewLecture = (data) => {
         console.log("SOCKET RESPONSE: ", data);
         lecture_controller.createLecture(data).then((ret) => {
