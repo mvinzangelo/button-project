4ff7e34405e00dff21b2cbf889b6868fcfcce1ea
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-07 17:22:51 -0700

    Add end lecture emit and disconnect students from lecture

--- backend/api/postgres/handers/studentHander.js
+++ backend/api/postgres/handers/studentHander.js
@@ -9,8 +9,15 @@ module.exports = (io, socket, lectures) => {
         const { name, code } = data;
         console.log(`${name} has joined lecture ${code}`);
         lectures.joinLecture(code, name);
+        socket.join(code);
+        console.log(socket.rooms);
+    }
+    const onLectureEnded = (data) => {
+        console.log("left lecture: ", data);
+        socket.leave(data)
     }
 
     socket.on("button_press", onConfusionButtonPress);
     socket.on("join_lecture", onJoinLecturePress);
+    socket.on("lecture_ended", onLectureEnded);
 }
