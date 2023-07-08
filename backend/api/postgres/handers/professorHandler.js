2e160ddb31651ceadba664e807e38d8cbafcc716
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-08 03:56:08 -0700

    Reformat professorHandler.js

--- backend/api/postgres/handers/professorHandler.js
+++ backend/api/postgres/handers/professorHandler.js
@@ -3,11 +3,12 @@ const lecture_controller = require('../controllers/lecture-controller');
 module.exports = (io, socket, lectures) => {
     const onCreateNewLecture = (data) => {
         console.log("SOCKET RESPONSE: ", data);
-        lecture_controller.createLecture(data).then((ret) => {
-            socket.emit('return_lecture_id', ret.dataValues.id);
-            lectures.addLecture(ret.dataValues);
-            console.log("------------ state------------\n", lectures.lecturesState);
-        });
+        lecture_controller.createLecture(data)
+            .then((ret) => {
+                socket.emit('return_lecture_id', ret.dataValues.id);
+                lectures.addLecture(ret.dataValues);
+                console.log("------------ state------------\n", lectures.lecturesState);
+            });
     }
 
     const onEndCurrentLecture = (data) => {
