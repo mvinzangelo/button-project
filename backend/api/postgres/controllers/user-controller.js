cd5940d7d105004a2281e86f6d53a1f5c68e1e6d
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-11 07:32:49 -0700

    Convert user-controller to createOrFind

--- backend/api/postgres/controllers/user-controller.js
+++ backend/api/postgres/controllers/user-controller.js
@@ -7,16 +7,18 @@ module.exports = {
             .then(token => token !== null)
             .then(idExists => idExists);
     },
-    createUser: function (user) {
-        return User.create({
-            id: user.id,
-            firstName: user.first_name,
-            lastName: user.last_name,
-            displayName: user.display_name,
-            email: user.email
+    findOrCreateUser: function (user) {
+        return User.findOrCreate({
+            where: { id: user.id },
+            defaults: {
+                firstName: user.first_name,
+                lastName: user.last_name,
+                displayName: user.display_name,
+                email: user.email
+            }
         })
             .then((data => {
-                console.log(data.toJSON());
+                console.log(data);
                 return data;
             }))
             .catch((err) => {
