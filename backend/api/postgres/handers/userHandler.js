3a3efa09fb9f9c857be07893e8c95f011e881a90
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-11 07:16:49 -0700

    Create getUserRole handler

--- backend/api/postgres/handers/userHandler.js
+++ backend/api/postgres/handers/userHandler.js
@@ -5,5 +5,16 @@ module.exports = (io, socket, lectures) => {
         user_controller.createUser(data);
     }
 
+    const getUserRole = (data) => {
+        user_controller.getUserByPk(data)
+            .then((user) => {
+                socket.emit('return_user_role', user.dataValues.role);
+            })
+            .catch((err) => {
+                console.error("Failed to get user: ", err);
+            });
+    }
+
     socket.on("create_user", onCreateUser)
+    socket.on("get_user_role", getUserRole)
 }
