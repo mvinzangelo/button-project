d4b283c923203806ecbe2a9112b44ad7dabd9774
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-07-14 01:15:48 -0700

    Show chart of confusion overtime on lecture end

--- frontend/src/components/ProfessorView.js
+++ frontend/src/components/ProfessorView.js
@@ -3,14 +3,14 @@ import { Route, Redirect, useLocation, useHistory } from "react-router-dom";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import Button from "react-bootstrap/Button";
 import io from 'socket.io-client'
-import "./StudentView.css";
+import "./ProfessorView.css";
 
 const socket = io.connect();
 
 export const ProfessorView = (props) => {
 
     const history = useHistory();
-    const binInterval = 5000; // interval of bins in millisecons (5 seconds)
+    const binInterval = 5000; // interval of bins in millisecons 
 
     const {
         user
@@ -18,6 +18,7 @@ export const ProfessorView = (props) => {
 
     const [lectureCode, setLectureCode] = useState('');
     const [lecturePresses, setLecturePresses] = useState([]);
+    const [graphData, setGraphData] = useState(null);
 
     useEffect(() => {
         socket.on('return_lecture_id', (res) => {
@@ -35,6 +36,22 @@ export const ProfessorView = (props) => {
         return bins;
     }
 
+    // from https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+    function millisToMinutesAndSeconds(millis) {
+        var minutes = Math.floor(millis / 60000);
+        var seconds = ((millis % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+
+    function convertHistData(data, binInterval) {
+        return data.map((x, index) => (
+            {
+                name: millisToMinutesAndSeconds(index * binInterval),
+                pv: x
+            }
+        ));
+    }
+
     const createNewLecturePress = () => {
         // TODO: add a check if they're already in a lecture
         console.log("=========Create new lecture button pressed=========");
@@ -44,6 +61,24 @@ export const ProfessorView = (props) => {
         socket.emit('create_new_lecture', data);
         history.push('/professor/in-lecture');
 
+    }
+    function ButtonGraph() {
+        return (
+            <ResponsiveContainer width="50%" height="50%">
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={graphData}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
+            </ResponsiveContainer>
+        )
     }
     const endCurrentLecturePress = () => {
         // TODO: get this lecture code from the backend
@@ -56,6 +91,10 @@ export const ProfessorView = (props) => {
             console.log(endTime);
             // create histogram for data
             const hist = histogram(lecturePresses, binInterval, endTime);
+            // set the data to be an array of objects 
+            const graph = convertHistData(hist, binInterval);
+            setGraphData(graph);
+
             console.log("histogram : ", hist);
         });
         history.push('/professor/create-lecture');
@@ -72,28 +111,10 @@ export const ProfessorView = (props) => {
             <div>
                 <Button onClick={endCurrentLecturePress}>End current lecture</Button>
             </div>
-            {/* <ResponsiveContainer width="100%" height="100%">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={[1, 1, 1, 10]}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                </LineChart>
-            </ResponsiveContainer> */}
             {/* </Route> */}
+            {/* <Route path='/professor/lecture-data'> */}
+            {/* </Route> */}
+            <ButtonGraph />
         </div>
     )
 }
